b96bb0b3c189e2b96ab53eb00b7ff909a3d036e5
KIUKIUTAM <a1998gogo@icloud.com>
2024-07-24 00:34:58 +0800

    update the product detail no test no test

--- dealer/product_Detail.php
+++ dealer/product_Detail.php
@@ -6,10 +6,6 @@ if(!isset($_SESSION['dealer'])){
   exit();
 }
 ?>
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,44 +15,19 @@ if(!isset($_SESSION['dealer'])){
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>SLMS - eCommerce Website</title>
 
-  <!--
-    - favicon
-  -->
   <link rel="shortcut icon" href="../assets/img/catHead.jpg" type="image/x-icon" />
-
-  <!--
-    - custom css link
-  -->
   <link rel="stylesheet" href="./assets/css/style-prefix.css" />
   <link rel="stylesheet" href="./assets/css/style-productDetail.css" />
   <link rel="stylesheet" href="./assets/css/toast.css" />
-  <!--
-    - google font link
-  -->
+  
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
 </head>
 
 <body>
-
-  <!--
-    - HEADER
-  -->
-
-  <header>
-
-  </header>
-
-  <!--
-    - MAIN
-  -->
-
+  <header></header>
   <main>
-    <!--
-      - PRODUCT
-    -->
-
     <div class="productDetail-container">
       <?php
       $sparePartNum = $_GET["sparePartNum"];
@@ -79,67 +50,66 @@ if(!isset($_SESSION['dealer'])){
             $categoryName = "Accessories";
             break;
         };
-        echo '<div class="container">
+      ?>
+        <div class="container">
           <div class="product-box">
             <div class="product">
               <a href="#" class="product-img-box">
-                <img
-                  src="' . $row["sparePartImage"] . '"
-                  alt="product-img-box"
-                  width="300"
-                  class="product-img"
-                />
-              </a>';
-        if ($row["stockItemQty"] >= 100) {
-          $stockItemStatus = "In Stock";
-          echo '<style>#stockItemStatus{color:green;}
+                <img src="<?php echo $row["sparePartImage"] ?>" alt="product-img-box" width="300" class="product-img" />
+              </a>
+              <?php
+              if ($row["stockItemQty"] >= 100) {
+                $stockItemStatus = "In Stock";
+                echo '<style>#stockItemStatus{color:green;}
                       </style>';
-        } else {
-          $stockItemStatus = "Out of Stock";
-          echo '<style>#stockItemStatus{color:red;}
+              } else {
+                $stockItemStatus = "Out of Stock";
+                echo '<style>#stockItemStatus{color:red;}
                     </style>';
-        };
-        echo '
-                    <div class="product-content">
-                    <label id="bold" >Spare ID:  </label><span class="product-title" id="sparePartNum">' . $row["sparePartNum"] . '</span><br>
-                    <label id="bold" >Spare Name:  </label><span class="product-title" id="sparePartName">' . $row["sparePartName"] . '</span><br>
-                    <label id="bold" >Weight:  </label><span class="product-title" id="weight">' . $row["weight"] . 'Kg </span><br>
-                    <label id="bold" >Stock Status:  </label><span class="product-title" id="stockItemStatus">' . $stockItemStatus . '</span><br>
-                    <br>
-                    <label id="bold" >Description:</label>
-                    <p class="product-description">
-                      ' . $row["sparePartDescription"] . '
-                    <br><br><div class="price-box">';
-        if ($row["discountPrice"] == null) {
-          $price = $row["price"];
-          echo '<div class="price-box">
-                    <p class="price" id="price">$' . $price . '</p>';
-        } else {
-          $price = $row["discountPrice"];
-          $delPrice = $row["price"];
-          echo '<div class="price-box" >
-                    <p class="price" id="price">$' . $price . '</p>
-                    <del>$' . $delPrice . '</del>';
-        }
-        echo '</div>
-                  <div class="quantity">
-                    <button onclick="quantityDecrement()">
-                      <svg fill="none" viewBox="0 0 24 24" height="14" width="14" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" stroke="#47484b" d="M20 12L4 12"></path>
-                      </svg>
-                    </button>
-                    <input id="quantityNumber"  type="number" value="1" min="0"/>
-                    <button onclick="quantityIncrement()">
-                      <svg fill="none" viewBox="0 0 24 24" height="14" width="14" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" stroke="#47484b" d="M12 4V20M20 12H4"></path>
-                      </svg>
-                    </button>
-                  </div>
+              };
+              ?>
+              <div class="product-content">
+                <label id="bold">Spare ID: </label><span class="product-title" id="sparePartNum"><?php echo $row["sparePartNum"] ?></span><br>
+                <label id="bold">Spare Name: </label><span class="product-title" id="sparePartName"><?php echo $row["sparePartName"] ?></span><br>
+                <label id="bold">Weight: </label><span class="product-title" id="weight"><?php echo $row["weight"] ?>Kg</span><br>
+                <label id="bold">Stock Status: </label><span class="product-title" id="stockItemStatus"><?php echo $stockItemStatus ?></span><br>
+                <br>
+                <label id="bold">Description:</label>
+                <p class="product-description">
+                  <?php echo  $row["sparePartDescription"] ?>
+                  <br><br>
+                <div class="price-box">
+                  <?php
+                  if ($row["discountPrice"] == null) {
+                    $price = $row["price"];
+                    echo "<div class='price-box'><p class='price' id='price'>$ $price</p>";
+                  } else {
+                    $price = $row["discountPrice"];
+                    $delPrice = $row["price"];
+                    echo "<div class='price-box'>
+                    <p class='price' id='price'>$$price</p>
+                    <del>$delPrice</del>";
+                  } ?>
+                </div>
+                <div class="quantity">
+                  <button onclick="quantityDecrement()">
+                    <svg fill="none" viewBox="0 0 24 24" height="14" width="14" xmlns="http://www.w3.org/2000/svg">
+                      <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" stroke="#47484b" d="M20 12L4 12"></path>
+                    </svg>
+                  </button>
+                  <input id="quantityNumber" type="number" value="1" min="0" />
+                  <button onclick="quantityIncrement()">
+                    <svg fill="none" viewBox="0 0 24 24" height="14" width="14" xmlns="http://www.w3.org/2000/svg">
+                      <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" stroke="#47484b" d="M12 4V20M20 12H4"></path>
+                    </svg>
+                  </button>
+                </div>
                 <button class="product-content-button" onclick="addToCart()">Add To Cart</button>
               </div>
-            </div>  
+            </div>
           </div>
-        </div>';
+        </div>
+      <?php
       }
       ?>
     </div>
